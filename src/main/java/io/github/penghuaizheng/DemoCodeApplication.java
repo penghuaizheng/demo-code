8685035bf4b40eed65d7df6dfb1a848bb37facb6
penghuaizheng <3100366252@qq.com>
2024-02-05 00:17:16 +0800

    springboot代码生成器

---
+++ src/main/java/io/github/penghuaizheng/DemoCodeApplication.java
@@ -0,0 +1,16 @@
+package io.github.penghuaizheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动程序
+ *
+ * @author penghuaizheng
+ */
+@SpringBootApplication
+public class DemoCodeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCodeApplication.class, args);
+    }
+}
